dd47058a6759fbdf707dcd6d7e1f7e8b34f2d3e3
ruslana-shev <ruslana.shevandina@gmail.com>
2017-03-11 02:05:05 +0200

    добавлен адаптив

---
+++ app/js/gulpfile.js
@@ -0,0 +1,11 @@
+;(function ($) {
+	var header = $('header');
+	$(window).on('scroll', function () {
+		if( $(window).scrollTop() > 20 ) {
+			header.addClass('on-scroll');
+		} 
+		else {
+			header.removeClass('on-scroll');
+		}
+	})
+}) (jQuery);
